41342d598d6c0c2305f9e9309f32e5b491930077
parth <parthnew3@gmail.com>
2024-10-23 16:25:11 +0530

    Adding seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# First, create an admin user
+admin_user = User.create!(
+  email: 'admin@yopmail.com',
+  password: '123456',
+  password_confirmation: '123456'
+)
+
+# Create 10 students and associate them with the admin user
+10.times do
+  Student.create!(
+    name: Faker::Name.name,
+    subject: Faker::Educator.subject,
+    marks: rand(50..100),  # Random marks between 50 and 100
+    user: admin_user       # Associate each student with the admin user
+  )
+end
+
+puts "Seed data successfully created!"
